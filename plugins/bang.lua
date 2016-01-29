1a2e353efa4922e5d7845ac28c5d55051665db3f
mrbangi <xboxlivemnasr@gmail.com>
2016-01-29 19:15:58 +0330

    Update bang.lua

--- plugins/bang.lua
+++ plugins/bang.lua
@@ -1,37 +1,13 @@
 do
 local function run(msg)
 if msg.text == "channel" then
-	return "@bangi_channel\n pokerface a private bot for @mrbangi_org"
+	return "@tele_pic\n pokerface a private bot for @mrbangi_org"
 end
-local function run(msg)
-if msg.text == "bangi"
-	return "joonam"
-end
-local function run(msg)
-if msg.text == "mrbangi" then
-	return "hum?!" 
-end
-local function run(msg)
-if msg.text == "hi" then
-	return "hi"
-end
-local function run(msg)
-if msg.text == "bye" then
-	return "bye"
-end
-local function run(msg)
-if msg.text == "id" then
-	return "'user#id'
-return {
 	description = "Chat With Robot Server", 
 	usage = "chat with robot",
 	patterns = {
-		"^[Hh]i$",
-		"^[Mm]rbangi$",
-		"^[Bb]angi$",
-		"^[Cc]hannel$",
-		"^[Bb]ye$",
-		"^[Ii]d$",
+		"^([Cc]hannel)$",
+		
 		}, 
 	run = run,
     --privileged = true,
