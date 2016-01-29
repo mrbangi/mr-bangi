4d847d3e807d426129e623833850d255208a88c1
mrbangi <xboxlivemnasr@gmail.com>
2016-01-29 19:21:48 +0330

    Update bang.lua

--- plugins/bang.lua
+++ plugins/bang.lua
@@ -3,10 +3,13 @@ local function run(msg)
 if msg.text == "channel" then
 	return "@tele_pic\n pokerface a private bot for @mrbangi_org"
 end
+if not is_momod(msg) then
+    	return "For moderators only!"
+  	end
 	description = "Chat With Robot Server", 
 	usage = "chat with robot",
 	patterns = {
-		"^([Cc]hannel)$",
+		"^[!/](channel)$",
 		
 		}, 
 	run = run,
