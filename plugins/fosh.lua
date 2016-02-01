fe565d6bc05261328799c4f542e72e670ea3d8ee
mrbangi <xboxlivemnasr@gmail.com>
2016-02-01 17:54:42 +0330

    Create fosh.lua

---
+++ plugins/fosh.lua
@@ -0,0 +1,14 @@
+do
+ local function run(msg, matches)
+if msg.text == "kos nane" then
+elseif matches[1] and not is_admin then
+ chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+description { del users when use trash
+patterns{
+"^!onservice
+"^[/!](kos nane)
+}
+)
+end
+   end
+
